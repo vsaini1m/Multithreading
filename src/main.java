64d95833145c46b07c23c2230bb27082addeb53b
sainivinit <vinitsaini357@gmail.com>
2019-08-15 18:40:47 +0530

    Multithreading with Anonymous class

--- src/main.java
+++ src/main.java
@@ -1,15 +1,35 @@
-class main{
+class main {
     public static void main(String[] args) {
-        thread1 obj1=new thread1();
-        thread2 obj2=new thread2();
+        Runnable obj1 = new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(" this is thread 1");
+                    try {
+                        Thread.sleep(500);
+                    } catch (Exception e) {
+                    }
+                }
 
-        Thread t1=new Thread(obj1);
-        Thread t2=new Thread(obj2);
+            }
+        }
+            ;
+            final Runnable obj2 = new Runnable() {
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println(" this is thread 2");
+                        try {
+                            Thread.sleep(500);
+                        } catch (Exception e) {
+                        }
+                    }
+
+                }};
+                Thread t1 = new Thread(obj1);
+                Thread t2 = new Thread(obj2);
         t1.start();
-        try {
-            Thread.sleep(10);
-        }catch (Exception e){}
+        try{Thread.sleep(10);}catch(Exception e){}
         t2.start();
-    }
+            }
 
-}
+        }
+
--- src/thread.java
+++
@@ -1,8 +0,0 @@
- class thread1 implements Runnable{
-    public void run(){
-        for (int i=0;i<10;i++){
-            System.out.println(" this is thread 1");
-            try{Thread.sleep(500);}catch (Exception e){}
-        }
-    }
-}
