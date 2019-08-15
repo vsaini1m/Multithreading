014dac7b2088e239c5419ecbde0736b2dc75ac35
sainivinit <vinitsaini357@gmail.com>
2019-08-15 18:51:07 +0530

    Multithreading winth lambds expration

--- src/main.java
+++ src/main.java
@@ -1,31 +1,28 @@
 class main {
     public static void main(String[] args) {
-        Runnable obj1 = new Runnable() {
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    System.out.println(" this is thread 1");
-                    try {
-                        Thread.sleep(500);
-                    } catch (Exception e) {
-                    }
-                }
 
-            }
-        }
-            ;
-            final Runnable obj2 = new Runnable() {
-                public void run() {
+                Thread t1 = new Thread(()-> {
                     for (int i = 0; i < 10; i++) {
-                        System.out.println(" this is thread 2");
+                        System.out.println(" this is thread 1");
                         try {
                             Thread.sleep(500);
                         } catch (Exception e) {
                         }
                     }
 
-                }};
-                Thread t1 = new Thread(obj1);
-                Thread t2 = new Thread(obj2);
+
+                }
+                        );
+                Thread t2 = new Thread(()-> {
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println(" this is thread 2");
+                        try {
+                            Thread.sleep(500);
+                        } catch (Exception e) {
+                        }
+
+
+                    }});
         t1.start();
         try{Thread.sleep(10);}catch(Exception e){}
         t2.start();
